77b54bfa2a32fb8c66a48a9e7e09cd8496b3d80f
yihao <yihao039@gmail.com>
2024-12-23 17:15:28 +0800

    Update axios instance to use backend URL for CORS header

--- front-end/src/api/axiosInstance.ts
+++ front-end/src/api/axiosInstance.ts
@@ -1,13 +1,13 @@
-import axios from "axios"
+import axios from "axios";
 // import viteConfig from "../../vite.config.js";
 
 const apiClient = axios.create({
-    baseURL: import.meta.env.VITE_BACKEND_URL,
-    timeout: 5000,
-    headers: {
-        "Access-Control-Allow-Origin": "*",
-        "Authorization": `Bearer ${localStorage.getItem("token")}`
-    }
+  baseURL: import.meta.env.VITE_BACKEND_URL,
+  timeout: 5000,
+  headers: {
+    "Access-Control-Allow-Origin": import.meta.env.VITE_BACKEND_URL,
+    Authorization: `Bearer ${localStorage.getItem("token")}`,
+  },
 });
 
 export default apiClient;
