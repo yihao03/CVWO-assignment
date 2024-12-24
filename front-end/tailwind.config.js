32e0d11d939b2d9cd1848d4caf14fce78fc2e288
yihao <yihao039@gmail.com>
2024-12-24 16:37:54 +0800

    Christmas update!

--- front-end/tailwind.config.js
+++ front-end/tailwind.config.js
@@ -7,11 +7,16 @@ export default {
   theme: {
     extend: {
       colors: {
-        primary: "#BFBFBF",
-        secondary: "#8C8C8C",
+        primary: "#667432",
+        secondary: "#A11D21",
         text: "#0D0D0D",
-        light: "#F2F2F2",
-        dark: "#404040",
+        light: "#F2F0D8",
+        dark: "#142715",
+        // primary: "#BFBFBF",
+        // secondary: "#8C8C8C",
+        // text: "#0D0D0D",
+        // light: "#F2F2F2",
+        // dark: "#404040",
       },
     },
     fontFamily: {
