c94c35255dae05b1e45e89f5cb7ee7ec57dca8f7
Hon Yi Hao <165232024+yihao03@users.noreply.github.com>
2024-12-25 22:30:56 +0700

    Update makePost.tsx

--- front-end/src/controllers/makePost.tsx
+++ front-end/src/controllers/makePost.tsx
@@ -40,7 +40,7 @@ function MakePost(props: MakePostDetails): React.ReactElement {
           response.data.post[0];
         const user = GetUserInfo();
         console.log("Editing post:", response.data.post[0]);
-        if (user && (user.userID !== Number(user_id) || !user.admin)) {
+        if (user && (user.userID !== Number(user_id) || user.admin)) {
           alert("You can only edit your own posts");
           navigate(-1);
           return;
